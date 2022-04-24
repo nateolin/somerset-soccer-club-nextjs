efe8ad67e00282dfb1e3ff16ac4b4cdeb98115fc
thenolin <nateolin17@gmail.com>
2022-04-24 17:57:56 -0500

    change nested hrefs

--- components/header.tsx
+++ components/header.tsx
@@ -20,22 +20,22 @@ import Image from 'next/image'
 const solutions = [
   {
     name: 'Mission',
-    href: 'about/mission',
+    href: '/about/mission',
     icon: SparklesIcon,
   },
   {
     name: 'Bylaws',
-    href: 'about/bylaws',
+    href: '/about/bylaws',
     icon: BadgeCheckIcon,
   },
   {
     name: 'Board Members & Positions',
-    href: 'about/board',
+    href: '/about/board',
     icon: ShieldCheckIcon,
   },
   {
     name: 'Meeting Minutes',
-    href: 'about/meeting-minutes',
+    href: '/about/meeting-minutes',
     icon: ClipboardListIcon,
   },
 ]
@@ -287,12 +287,12 @@ export default function Example() {
           </Popover.Group>
           {!user && (
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-              <Link href="account/login">
+              <Link href="/account/login">
                 <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                   Sign in
                 </a>
               </Link>
-              <Link href="account/register">
+              <Link href="/account/register">
                 <a className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700">
                   Register
                 </a>
