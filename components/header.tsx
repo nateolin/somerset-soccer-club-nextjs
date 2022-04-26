85e7fcbe6644444f0d10e939c7cc28d4ec46bb6e
thenolin <nateolin17@gmail.com>
2022-04-26 08:44:37 -0500

    rename

--- components/header.tsx
+++ components/header.tsx
@@ -80,7 +80,7 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Header() {
   const [user, setUser] = useState(null)
 
   return (
