5a56b5b42919de2ec835a78571ffeb417b9d8cfc
thenolin <nateolin17@gmail.com>
2022-04-26 15:09:47 -0500

    build out reusable input with label

--- components/tailwind-components/input.tsx
+++ components/tailwind-components/input.tsx
@@ -1,15 +1,30 @@
-export const InputWithLabel = (
-  name: string,
-  label: string,
-  placeholder: string,
-  defaultValue: string
-) => {
+import { Path, UseFormRegister } from 'react-hook-form'
+import { UserRegistration } from '../../pages/account/register'
+
+interface InputWithLabelProps {
+  name: Path<UserRegistration>
+  label: string
+  register: UseFormRegister<UserRegistration>
+  required: boolean
+  placeholder?: string
+  defaultValue?: string
+}
+
+export const InputWithLabel = ({
+  name,
+  label,
+  register,
+  required,
+  placeholder,
+  defaultValue,
+}: InputWithLabelProps) => {
   return (
     <div>
       <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700 ">
         {label}
       </label>
       <input
+        {...register(name, { required })}
         placeholder={placeholder}
         defaultValue={defaultValue}
         className="mb-3 block w-full appearance-none rounded border py-3 px-4 leading-tight text-gray-800 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500"
