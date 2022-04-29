4a170e6e7f1a3c838fb87be3206e5835cc5469c7
thenolin <nateolin17@gmail.com>
2022-04-29 16:05:56 -0500

    create reusable button components

---
+++ components/tailwind-components/tertiaryButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const TertiaryButton = () => {
+  return (
+    <button
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-md 
+      px-4 py-2 text-base font-medium text-cyan-700 hover:bg-cyan-100 hover:shadow-sm 
+      focus:bg-cyan-100 focus:shadow-sm focus:outline-none focus:ring-0 active:bg-cyan-200 
+      active:shadow-sm"
+    >
+      Add Child
+    </button>
+  )
+}
