6c58f03e871d8c8c4cff89aa955f05a31f58c2fc
thenolin <nateolin17@gmail.com>
2022-05-27 14:38:42 -0500

    add on click fn to button

--- components/tailwind-components/secondaryButton.tsx
+++ components/tailwind-components/secondaryButton.tsx
@@ -1,12 +1,14 @@
 interface SecondaryButtonProps {
+  onClick: any
   text: string
 }
 
-export const SecondaryButton = ({ text }: SecondaryButtonProps) => {
+export const SecondaryButton = ({ onClick, text }: SecondaryButtonProps) => {
   return (
     <button
       className="whitespace-nowrap rounded-md border border-red-600 px-4 py-2 text-base font-medium text-red-600 hover:border-red-700 hover:text-red-700 active:border-red-800 active:text-red-800"
       type="button"
+      onClick={onClick}
     >
       {text}
     </button>
