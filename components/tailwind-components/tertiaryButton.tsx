f86774ea168ef0e88798711e88522a42daed2f1f
thenolin <nateolin17@gmail.com>
2022-05-27 10:16:59 -0500

    externalize onClick and text params for tert button

--- components/tailwind-components/tertiaryButton.tsx
+++ components/tailwind-components/tertiaryButton.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
-export const TertiaryButton = () => {
+interface TertiaryButtonProps {
+  onClick: any
+  text: string
+}
+
+export const TertiaryButton = ({ onClick, text }: TertiaryButtonProps) => {
   return (
     <button
       className="inline-flex items-center justify-center whitespace-nowrap rounded-md 
       px-4 py-2 text-base font-medium text-cyan-700 hover:bg-cyan-100 hover:shadow-sm 
       focus:bg-cyan-100 focus:shadow-sm focus:outline-none focus:ring-0 active:bg-cyan-200 
       active:shadow-sm"
+      onClick={onClick}
     >
-      Add Child
+      {text}
     </button>
   )
 }
